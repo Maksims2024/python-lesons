e7e4c9532503cf82a633aa01c2593983a544c18f
Maksim S <maksimsmirnov0807@gmail.com>
2024-02-10 14:18:54 +0200

    lesson2

---
+++ p02/exercise1.py
@@ -0,0 +1,35 @@
+import sys
+import datetime as dt
+
+# print('Area of circle')
+# radius = int(input('Input radius: '))
+# pi = 3.1415
+# area = pi * radius ** 2
+# print('Area of the circle = ', area)
+
+# print('Volume of the sphere')
+# radius = int(input('Input radius: '))
+# pi = 3.1415
+# volume = 4 / 3 * pi * radius ** 3
+# print(f'Volume of the sphere {volume}')
+
+# a = input('Input an integer: ')
+# a1 = a
+# a2 = a + a
+# a3 = a + a + a
+# sum = int(a1) + int(a2) + int(a3)
+# print(sum)
+
+# print("Python version")
+# print(sys.version)
+# print("Version info")
+# print(sys.version_info)
+
+# now = dt.datetime.now()
+# print(now.strftime('%Y-%m-%d %H:%M:%S'))
+
+# UNICODE
+# \x1b[0;32m
+print('\x1b[0;32m\u2591\x1b[0;30m'*40)
+print('\u2592'*12, ' Unicode text ', '\u2592'*12)
+print('\u2593'*40)
